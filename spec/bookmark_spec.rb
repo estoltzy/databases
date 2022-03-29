22fcdb8517dc272f997a1caa0e88504ef0101c16
Eleanor Chamberlain-Stoltzfus <eleanor.stoltzfus@gmail.com>
2022-03-29 14:29:58 +0100

    added create method and updated app and tests

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -19,3 +19,10 @@ describe '.all' do
   end
 end
 
+describe '.create' do
+  it 'creates a new bookmark' do
+    Bookmark.create(url: 'http://www.example.org')
+    expect(Bookmark.all).to include 'http://www.example.org'
+  end
+end
+
